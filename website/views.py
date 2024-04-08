1f87278d69e6f799cd5cd6f20899f8162a8980e5
David Lartey <larteydavid59@gmail.com>
2024-04-08 07:19:33 +0000

    updates

--- website/views.py
+++ website/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html', {})
+
+## Authentication
+
+# Registeration
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        
+        if password == confirm_password:
+            if User.objects.filter(username=username, email=email).exists():
+                messages.info(request, 'User already registered')
+                return redirect('register')
+            else:
+                user = User.objects.create(username=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Password do not match')
+            return redirect('register')
+    else:    
+        return render(request, 'register.html')
+
+# Login Authentication
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+
+# Logout Authentication
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
